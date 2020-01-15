ea8224599401332f4ede2d3be9a923c3f7ed09ba
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2020-01-15 13:38:32 +0530

    server side render implementation

--- script/app.js
+++ script/app.js
@@ -2,53 +2,48 @@
 // let users = [];
 
 // const evaluate = event => {
-//   // event.preventDefault();
+//   event.preventDefault();
 //   let email1 = document.getElementById("email1").value;
 //   let email2 = document.getElementById("email2").value;
-//   console.log(email1, email2);
 //   if (email1 === email2) {
-//     return;
+//     console.log("Success");
 //   } else {
-//     return;
+//     console.log("Not Same");
 //   }
 
-//   let pass1 = document.getElementById("password1").value;
-//   let pass2 = document.getElementById("password2").value;
-//   if (pass1 === pass2) {
-//     return;
+//   function getAge() {
+//     var dateString = document.getElementById("date").value;
+//     console.log(dateString);
+//     if (dateString != "") {
+//       var today = new Date();
+//       var birthDate = new Date(dateString);
+//       var age = today.getFullYear() - birthDate.getFullYear();
+//       var m = today.getMonth() - birthDate.getMonth();
+//       var da = today.getDate() - birthDate.getDate();
+//       if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+//         age--;
+//       }
+//       if (m < 0) {
+//         m += 12;
+//       }
+//       if (da < 0) {
+//         da += 30;
+//       }
+
+//       if (age < 18 || age > 100) {
+//         alert("Age " + age + " is restrict");
+//       }
+//     } else {
+//       alert("please provide your date of birth");
+//     }
+//     return true;
 //   }
-//   //   setTimeout(() => {
-//   //     document.querySelector("#myForm").submit;
-//   //     window.location.replace("index.html");
-//   //   }, 1000);
-//   //   let gender;
-//   //   if (document.getElementById("male").checked) {
-//   //     gender = document.getElementById("male").value.toUpperCase();
+//   const valid = getAge();
+//   console.log(valid);
+//   //   if (valid) {
+//   //     document.querySelector("#myForm").submit();
+//   //     console.log("success");
 //   //   }
-//   //   if (document.getElementById("female").checked) {
-//   //     gender = document.getElementById("female").value.toUpperCase();
-//   //   }
-//   //   if (document.getElementById("other").checked) {
-//   //     gender = document.getElementById("other").value.toUpperCase();
-//   //   }
-//   //   let DOB = document.getElementById("date").value;
-//   //   let year = DOB.substr(0, 4);
-//   //   let month = DOB.substr(5, 2);
-//   //   let date = DOB.substr(8, 2);
-//   //   let formatted_DOB = date + "." + month + "." + year;
-//   //   let user = {
-//   //     firstName: document.getElementById("fname").value,
-//   //     lastName: document.getElementById("lname").value,
-//   //     gender: gender,
-//   //     DOB: formatted_DOB,
-//   //     address1: document.getElementById("address1").value,
-//   //     address2: document.getElementById("address2").value,
-//   //     email: document.getElementById("email1").value,
-//   //     password: document.getElementById("password1").value
-//   //   };
-//   //   users.push(user);
-//   //   console.log(users);
-//   //   setTimeout(5000);
 // };
 
 // document.getElementById("btn").addEventListener("click", evaluate);
--- server.js
+++ server.js
@@ -1,40 +1,50 @@
 const express = require("express");
 const fs = require("fs");
 const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.engine("html", require("ejs").renderFile);
+app.use("/assets", express.static(path.join(__dirname, "/assets")));
+app.use("/script", express.static(path.join(__dirname, "/script")));
 
 let data = fs.readFileSync("./file.json");
 let students = JSON.parse(data);
 
-const app = express();
-
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-app.engine("html", require("ejs").renderFile);
-
-app.get("/sign", (req, res) => {
-  res.redirect("localhost:5500/sign.html");
+app.get("/", (req, res) => {
+  res.render("index.html");
 });
 
+app.get("/contact", (req, res) => {
+  res.render("contact.html");
+});
+
+app.get("/admin", (req, res) => {
+  res.render("admin.html");
+});
+
+app.get("/sign", (req, res) => {
+  res.render("sign.html");
+});
 app.post("/sign", urlencodedParser, (req, res) => {
+  const random = Math.floor(Math.random() * (10000000 - 1000000) + 1000000);
+
   let student = {
-    // id: req.body.fname.slice(1, 3) + time,
+    id: random,
     firstName: req.body.fname,
     lastName: req.body.lname,
     gender: req.body.gender,
     DOB: req.body.bday,
     address1: req.body.address1,
     address2: req.body.address2,
-    email1: req.body.email1,
-    pass: req.body.password1
+    email1: req.body.email1
   };
   students.push(student);
   let data = JSON.stringify(students, null, 2);
-
-  fs.writeFile("./file.json", data, err => console.log("success"));
-  res.redirect("localhost:5500/index.html");
-});
-
-app.get("/", (req, res) => {
-  res.redirect("localhost:5500/index.html");
+  fs.writeFile("./file.json", data, err => console.log(student.id));
+  res.redirect("/");
 });
 
 const PORT = process.env.PORT || 3000;
